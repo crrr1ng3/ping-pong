c0338f26d11e11ccfeeda51b8a258cf0f5bbec13
crrr1ng3 <138374080+crrr1ng3@users.noreply.github.com>
2023-07-16 14:50:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ font.init()
 win_width, win_height = 700, 500
 window = display.set_mode((win_width, win_height))
 window.fill((51, 255, 255))
+display.set_caption('Ping-Pong')
 clock = time.Clock()
 FPS = 60
 
@@ -38,14 +39,17 @@ class Player(GameSprite):
         if keys[K_s] and self.rect.y < 350:
             self.rect.y += self.speed
 
-
-class Ball(GameSprite):
-    def update(self):
-        return
+speed_x, speed_y = 3, 3
     
+
 player1 = Player('rocket.png', 15, 60, 250, 35, 150)
 player2 = Player('rocket.png', 15, 600, 250, 35, 150)
-ball = Ball('ball.png', 10, 305, 205, 50, 50)
+ball = GameSprite('ball.png', 10, 305, 205, 50, 50)
+
+font1 = font.Font(None, 35)
+lose1 = font1.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font1.render('PLAYER 2 LOSE!', True, (180, 0, 0))
+
 
 while run:
     window.fill((51, 255, 255))
@@ -60,6 +64,25 @@ while run:
 
         player2.reset()
         player2.update_r()
+
+        ball.reset()
+
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *= -1
+
+        if sprite.collide_rect(player1, ball) or sprite.collide_rect(player2, ball):
+            speed_x *= -1
+
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200, 200))
+        
+        if ball.rect.x > 700:
+            finish = True
+            window.blit(lose2, (200, 200))
 
 
         clock.tick(FPS)
